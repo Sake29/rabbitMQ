42eedcdd234142795fdcff997eed9b31f62d56ac
sake <514679708@qq.com>
2020-09-23 17:37:01 +0800

    修改了一些小的问题

--- Utils/src/main/java/ConnectionUtil.java
+++ Utils/src/main/java/ConnectionUtil.java
@@ -18,7 +18,7 @@ public class ConnectionUtil {
         //端口
         factory.setPort(5672);
         //设置账号信息，用户名、密码、vhost
-        //factory.setVirtualHost("/kavito");//设置虚拟机，一个mq服务可以设置多个虚拟机，每个虚拟机就相当于一个独立的mq
+        //factory.setVirtualHost("/sake");//设置虚拟机，一个mq服务可以设置多个虚拟机，每个虚拟机就相当于一个独立的mq
         factory.setUsername("guest");
         factory.setPassword("guest");
         //通过工厂获取连接
